25c5b5ff258d4a000bcfe54b85e64705901de4cc
sieckWork <cesar.trevino.work.com>
2024-04-07 06:03:53 -0400

    add gallery

---
+++ js.js
@@ -0,0 +1,50 @@
+'use strict'
+// module Memoire
+const Portfolio = (function () {
+
+
+    function createElement(tagName, attributes) {
+        const element = document.createElement(tagName)
+
+        for (let i = 0; i < attributes.length; i++) {
+            element.setAttribute(attributes[i].name, attributes[i].value)
+        }
+
+        return element
+    }
+
+    function fillGallery(container) {
+
+        let div = createElement('div', [{ name: 'class', value: 'gallery' }, { name: 'href', value: 'img/02.png' }]);
+
+        for (let index = 0; index < 15; index++) {
+            
+            const a = createElement('a', [{name:'href',value:'www.gogle.com'},{name:'taget',value:'_blank'}]);
+            const img = createElement('img', [{ name: 'src', value: 'img/0'+index+'.png' },{name:'class',value:'item'}]);
+    
+            a.appendChild(img);
+            div.appendChild(a);
+            
+        }
+        
+        return div;
+    };
+
+    return {
+        init: function (container) {
+            console.log("On");
+            let items = fillGallery();
+            container.appendChild(items);
+
+        }
+    }
+})()
+
+
+window.addEventListener('DOMContentLoaded', function loaded(event) {
+    window.removeEventListener('DOMContentLoaded', loaded, false)
+
+    // instance d'object Module memoire
+    Portfolio.init(document.getElementById('portfolio'));
+
+}, false)
