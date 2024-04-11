5d203717b5609b5b1a8a2019350279eb705a1943
sieckWork <cesar.trevino.work.com>
2024-04-11 06:21:17 -0400

    update gallery

--- js.js
+++ js.js
@@ -18,16 +18,16 @@ const Portfolio = (function () {
         let div = createElement('div', [{ name: 'class', value: 'gallery' }]);
 
         for (let index = 0; index < 15; index++) {
-            
-            const a = createElement('a', [{name:'href',value:'www.gogle.com'},{name:'taget',value:'_blank'},{name:'class',value:'item'}]);
+
+            const a = createElement('a', [{ name: 'href', value: 'www.gogle.com' }, { name: 'taget', value: '_blank' }, { name: 'class', value: 'item' }]);
             // const img = createElement('img', [{ name: 'src', value: 'img/0'+index+'.png' },{name:'class',value:'item'}]);
-               const img = createElement('img', [{ name: 'src', value: 'img/0'+index+'.png' }]);
-    
+            const img = createElement('img', [{ name: 'src', value: 'img/0' + index + '.png' }]);
+
             a.appendChild(img);
             div.appendChild(a);
-            
+
         }
-        
+
         return div;
     };
 
@@ -41,11 +41,72 @@ const Portfolio = (function () {
     }
 })()
 
+'use strict'
+// module Memoire
+const Slider = (function () {
+
+    let IMG = 0;
+
+    function nextImage() {
+
+        let next = document.getElementById("next");
+
+        next.addEventListener("click", function () {
+
+            if(IMG > 14){
+                IMG = 0;
+            }
+            changeImg();
+            IMG++;
+
+
+        });
+
+    };
+
+    function backImage() {
+
+        let next = document.getElementById("back");
+
+        next.addEventListener("click", function () {
+
+
+            if(IMG <= 0){
+                IMG = 15;
+            }
+
+            IMG--;
+
+
+            changeImg();
+
+        });
+
+    };
+
+    function changeImg() {
+
+        let img = document.getElementById("slider-img");
+
+        img.src = 'img/0' + IMG + '.png';
+
+    }
+
+    return {
+        init: function (container) {
+            nextImage();
+            backImage();
+        }
+    }
+
+})()
 
 window.addEventListener('DOMContentLoaded', function loaded(event) {
     window.removeEventListener('DOMContentLoaded', loaded, false)
 
     // instance d'object Module memoire
-    Portfolio.init(document.getElementById('portfolio'));
+    Portfolio.init(document.getElementById('slider'));
+    Slider.init(document.getElementById('slider'));
+
 
 }, false)
